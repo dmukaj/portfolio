8d24b19c98192ac481f7e49f2fbe59e59de135ec
Dajana Mukaj <dajana@Dajanas-MacBook-Pro.local>
2025-02-21 21:19:24 -0500

    added animation for theme icons

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -4,11 +4,12 @@ import { FaSun } from "react-icons/fa";
 import { useTheme } from "next-themes";
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
+  if (!resolvedTheme) return null;
 
   return (
-    <div className="absolute top-8 right-0 text-black dark:text-white ">
-      {theme && theme === "dark" ? (
+    <div className="absolute top-4 right-2 text-black dark:text-white ">
+      {resolvedTheme && resolvedTheme === "dark" ? (
         <FaMoon
           className="h-[1.5rem] w-[1.5rem] md:h-[2rem] md:w-[2rem] animate-fadeIn transition-transform"
           onClick={() => setTheme("light")}
