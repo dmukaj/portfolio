ba3a637805dd00c5916d24fde52d4283eab1a79b
Dajana Mukaj <dajana@Dajanas-MacBook-Pro.local>
2025-03-13 13:50:20 -0400

    updated index.ts and movingBoarder.tsx

--- components/ui/MovingBoarder.tsx
+++ components/ui/MovingBoarder.tsx
@@ -47,7 +47,7 @@ export function Button({
         <MovingBorder duration={duration} rx="30%" ry="30%">
           <div
             className={cn(
-              "h-32 w-20 opacity-[0.9] dark:bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)] ",
+              "h-32 w-20 opacity-[0.9] dark:bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]",
               borderClassName
             )}
           />
